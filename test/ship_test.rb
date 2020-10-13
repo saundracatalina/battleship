e8d95953d758a88c01b3a26dce9e63edbba3ddbf
Saundra <saundracatalina@gmail.com>
2020-10-13 17:25:46 -0600

    Add working test_it_exists_and_has_attributes

---
+++ test/ship_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+require 'pry'
+
+class ShipTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    cruiser = Ship.new("Cruiser", 3)
+    assert_instance_of Ship, cruiser
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+  end
+end

75dc4f74f1fc2d4ee508f202ccdea6b8ad5254da
Saundra <saundracatalina@gmail.com>
2020-10-13 20:33:06 -0600

    Add passing test_cell_can_be_fired_upon_if_has_ship

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,6 +3,7 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -15,6 +16,15 @@ class Cell
 
   def place_ship(ship)
     @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @ship.hit
+    @fired_upon = true  
   end
 
 end

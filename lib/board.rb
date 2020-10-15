473210d190c8ebde2f0b67db6351e3e2349b7e1b
Michael Foy <foymikek@gmail.com>
2020-10-15 15:25:27 -0600

    Add refactored method empty?

--- lib/board.rb
+++ lib/board.rb
@@ -33,7 +33,31 @@ class Board
     cell_coord
   end
 
+  def valid_coordinate?(coord)
+    @cells.keys.include?(coord)
+  end
 
+  def valid_placement(ship, placement_range)
+    #1. could then make a && with all helper methods. (2)
+    helper_method1?
+    helper_method2?
+    helper_method3?
+    helper_method4?
+    #2.
+    helper_method1? && helper_method2? && helper_method3? && helper_method4?
+  end
+  def helper_method1?
+    conditionA
+  end
+  def helper_method2?
+    conditionB
+  end
+  def helper_method3?
+    conditionC
+  end
+  def helper_method4?
+    conditionD
+  end
 
 
 end

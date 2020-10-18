752321e14394cccfbc8af817212e0ff4c01faa81
Michael Foy <foymikek@gmail.com>
2020-10-18 16:17:22 -0600

    Add working test_computer_can_generate_random_ship_placement

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -69,4 +69,18 @@ class ComputerTest < Minitest::Test
     assert_equal expected , computer.horizontal_and_vertical_placements(ship1)
   end
 
+  def test_computer_can_generate_random_ship_placement
+    computer = Computer.new
+    ship1 = Ship.new("Cruiser", 3)
+
+    expected = [["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"],
+    ["D1", "D2", "D3"], ["A2", "A3", "A4"], ["B2", "B3", "B4"],
+    ["C2", "C3", "C4"], ["D2", "D3", "D4"], ["A1", "B1", "C1"],
+    ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"],
+    ["B1", "C1", "D1"], ["B2", "C2", "D2"], ["B3", "C3", "D3"],
+    ["B4", "C4", "D4"]]
+    # require "pry"; binding.pry
+    assert expected.include?(computer.random_placement(ship1))
+  end
+
 end

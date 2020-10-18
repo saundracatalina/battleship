a15fa7371f2a7028620051c5c58a921e7034681a
Saundra <saundracatalina@gmail.com>
2020-10-18 17:13:35 -0600

    Changes spacing in assertions

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -9,9 +9,11 @@ class ComputerTest < Minitest::Test
   def test_it_exists_and_has_attributes
     computer = Computer.new
     assert_instance_of Computer, computer
+
     computer.ships.each do |ship|
       assert Ship, ship.class
     end
+
     assert Board, computer.board.class
   end
 

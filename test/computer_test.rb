5bf76c0e015712b373d7783d3ec931a09a4910f3
Michael Foy <foymikek@gmail.com>
2020-10-18 16:01:50 -0600

    Add working test_computer_can_generate_possible_vertical_ship_placement

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -40,4 +40,14 @@ class ComputerTest < Minitest::Test
     assert_equal expected, computer.generate_ship_coordinate_placement_horizontal(ship2)
   end
 
+  def test_computer_can_generate_possible_vertical_ship_placement
+    computer = Computer.new
+    ship1 = Ship.new("Cruiser", 3)
+
+    expected = [["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"],
+    ["A4", "B4", "C4"], ["B1", "C1", "D1"], ["B2", "C2", "D2"],
+    ["B3", "C3", "D3"], ["B4", "C4", "D4"]]
+    assert_equal expected, computer.generate_ship_coordinate_placement_vertical(ship1)
+  end
+
 end

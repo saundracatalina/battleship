7eade0e8547da3d0931803acd11bd833cd99030b
Michael Foy <foymikek@gmail.com>
2020-10-18 15:56:42 -0600

    Add passing test_computer_can_generate_possible_horizontal_ship_placement

--- lib/computer.rb
+++ lib/computer.rb
@@ -15,6 +15,21 @@ class Computer
     ships
   end
 
-
+  def generate_ship_coordinate_placement_horizontal(ship)
+    possible_number_placements  = []
+    (1..4).each_cons(ship.length) do |consec_nums|
+      possible_number_placements << consec_nums
+    end
+    # possible_number_placements = [[1, 2, 3], [2, 3, 4]]
+    possible_horizontal_placements = Array.new
+    possible_number_placements.each do |num_placement|
+      (1..4).each do |num|
+        possible_horizontal_placements  << num_placement.map do |column_num|
+          (num + 64).chr + column_num.to_s
+        end
+      end
+    end
+    possible_horizontal_placements
+  end
 
 end
